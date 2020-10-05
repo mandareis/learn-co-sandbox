5298aa7064f4f3b6d4c78c763b7add1c53c034e4
Amanda Reis <amanda@depaula.xyz>
2020-10-05 17:55:21 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,18 @@
+# puts "Hello World"
+# puts "hi"
+# hi
+
+# n = 5
+# count = 0 
+# while count < n do
+#   puts "I ran."
+#   count += 1 
+# end
+
+5.times do
+  puts "I ran"
+end
+
+10.times do
+  puts "I love you forever, my weirdo!"
+end
